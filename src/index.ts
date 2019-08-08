753eadd14038614e2609f2a4126a7a470455510c
马金鑫 <majinxin@bicai365.com>
2019-08-08 16:49:13 +0800

    公共的交易金额键盘和验证码的键盘

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import BcHeader from './component/BcHeader'
 import Bottom from './component/Bottom/index'
+import BcDealInput from './component/BcDealInput'
+import BcYzmInput from './component/BcYzmInput'
 export {
   BcHeader,
-  Bottom
+  Bottom,
+  BcDealInput,
+  BcYzmInput
 }
