3dfc3d48dacec5d594c4e3b5eff9ea31ee4b4781
iceyang <w@icey.cc>
2019-08-08 16:53:39 +0800

    提交代码

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
-
-
-export function goBank(){
-
+import {Native} from './appBridge'
+import isEquipment from './checkiserAgemt'
+export function goBank() {
 }
+export const BcAppBridge = Native
+export const BcEquipment = isEquipment
+
 
