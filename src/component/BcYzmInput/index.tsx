131dcf1c7c7c7d92bdfb45a4fe4be877241ce207
gaoxinxiao <gaoxinxiao@bicai365.com>
2019-09-02 13:57:39 +0800

    修改bug

--- src/component/BcYzmInput/index.tsx
+++ src/component/BcYzmInput/index.tsx
@@ -26,70 +26,72 @@ class BcYzmInput extends React.Component<Props, any>{
             flagTime: false
         }
     }
-    endTime = null
-    componentWillUnmount(): void {
-        clearInterval(this.endTime)
-    }
+
     componentWillReceiveProps(props) {
-        if (props.errHero) {
-            clearInterval(this.endTime)
-            this.props.ininState()
-            this.setState({
-                flagTime: false,
-                time: this.props.timer
-            })
+        console.log(props)
+        if (props.countDownFlag) {
+            this.actionCountDown()
         }
+        this.setState({
+            flagTime: props.countDownFlag,
+        })
     }
     //倒计时开始
     actionCountDown() {
         let { time } = this.state
-        let { timer } = this.props
-        this.setState({ flagTime: true })
+        let { timer,resetFlag } = this.props
 
-        this.endTime = setInterval(() => {
+        let endTime = setInterval(() => {
             this.setState({
                 time: time -= 1
+            },()=>{
+                if (time <=0) {
+                    clearInterval(endTime)
+                    endTime = null
+                    this.setState({ flagTime: false, time: timer })
+                    resetFlag(false)
+                }
             })
-            if (time == 0) {
-                clearInterval(this.endTime)
-                this.setState({ flagTime: false, time: timer })
-            }
         }, 1000)
     }
 
     componentDidMount() {
-        let { countDownFlag } = this.props
+        let { countDownFlag,timer } = this.props
         if (countDownFlag) {
             this.actionCountDown()
         }
+        this.setState({
+            flagTime: countDownFlag,
+            time:timer
+        })
     }
 
     render() {
         const isIOS = (Native.getSystemType() === 'ios' ? true : false)
-        let { click, change, errHero } = this.props
+        let { click, change, countDownFlag } = this.props
         let { time, flagTime } = this.state
         return (
             isIOS ?
-            <div className='BcYzm'>
-                <span>
-                    <InputItem maxLength={6} onChange={(e) => change(e, flagTime)} type="number" placeholder="输入验证码">验证码</InputItem>
-                </span>
-                <p>
-                    <i className='line'></i>
-                    <Button className='clearStyle' disabled={flagTime} onClick={() => click(this.actionCountDown.bind(this))}><span>{(flagTime ? `${time}秒后重发` : <span style={{ color: '#508CEE' }}>重新获取</span>)}</span></Button>
-                </p>
-            </div> :
-            <div className="yzm-box">
-                <BcNumberInput
-                    className="yzm-input"
-                    labelNumber={4}
-                    maxLength={6}
-                    onChange={(value) => change(value, flagTime)}
-                    placeholder="输入验证码"
-                    extra={`|<div id=${flagTime ? 'timer' : 'retrieve'}>${flagTime ? time + '秒后重发' : '重新获取'}</div>`}
-                    onExtraClick={flagTime ? () => {} : () => click(this.actionCountDown.bind(this))}
-                >验证码</BcNumberInput>
-            </div>
+                <div className='BcYzm'>
+                    <span>
+                        <InputItem maxLength={6} onChange={(e) => change(e, flagTime)} type="number" placeholder="输入验证码">验证码</InputItem>
+                    </span>
+                    <p>
+                        <i className='line'></i>
+                        <Button className='clearStyle' disabled={flagTime} onClick={() => click(this.actionCountDown.bind(this))}><span>{(flagTime ? `${time}秒后重发` : <span style={{ color: '#508CEE' }}>重新获取</span>)}</span></Button>
+                    </p>
+                </div> :
+                <div className="yzm-box">
+                    <BcNumberInput
+                        className="yzm-input"
+                        labelNumber={4}
+                        maxLength={6}
+                        onChange={(value) => change(value, flagTime)}
+                        placeholder="输入验证码"
+                        extra={`|<div id=${flagTime ? 'timer' : 'retrieve'}>${flagTime ? time + '秒后重发' : '重新获取'}</div>`}
+                        onExtraClick={flagTime ? () => { } : () => click(this.actionCountDown.bind(this))}
+                    >验证码</BcNumberInput>
+                </div>
         )
     }
 }
